6c1f3a4e4b392bb6c52dca070a82e77189f136c9
Piyada <Piyadakh@nvc.ac.th>
2019-10-06 15:07:38 +0700

    lab7

--- src/lab7/ex2.java
+++ src/lab7/ex2.java
@@ -6,40 +6,33 @@ public class ex2 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
+        int score;
 
-        System.out.println("Enter your score, " + "and I will tell you grade:");
+        do {
+            System.out.println("Enter your score, " + "and I will tell you grade:");
 
-        int score = sc.nextInt();
+            score = sc.nextInt();
 
-        if (score >= 0 && score <= 49 )
+            if (score >= 0 && score <= 49)
             System.out.println("Grade F.");
-        else  if (score >= 50 && score <= 54)
+            else if (score >= 50 && score <= 54)
             System.out.println("Grade D.");
-        else  if (score >= 55 && score <= 59)
+            else if (score >= 55 && score <= 59)
             System.out.println("Grade D+.");
-        else  if (score >= 60 && score <= 64)
+            else if (score >= 60 && score <= 64)
             System.out.println("Grade C.");
-        else  if (score >= 65 && score <= 69)
+            else if (score >= 65 && score <= 69)
             System.out.println("Grade C+.");
-        else  if (score >= 70 && score <= 74)
+            else if (score >= 70 && score <= 74)
             System.out.println("Grade B.");
-        else  if (score >= 75 && score <= 79)
+            else if (score >= 75 && score <= 79)
             System.out.println("Grade B+.");
-        else if (score >= 80 && score <= 100)
+            else if (score >= 80 && score <= 100)
             System.out.println("Grade A.");
-        else
-            System.out.println("กรุณากรอกจำนวนตัวเลขให้ถูกต้อง");
+            else
+            System.out.println("Please, enter score between 0-100 ");
 
-
-
-
-
-
-
-
-
-
-
+        } while (!(score >=0 && score <=100));
 
     }
 }
---
+++ src/lab7/ex5.java
@@ -0,0 +1,25 @@
+package lab7;
+
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int totalEven =0;
+        int totalOdd =0;
+        int x = 0;
+        for (int i = 1; i <= 10 ; i++) {
+
+            System.out.print("Enter an integer: ");
+            x = sc.nextInt();
+            if (x % 2 == 0)
+                totalEven += x; //total = total+x;
+            else
+                totalOdd += x;
+
+        }
+        System.out.println("Total of even number: "+totalEven);
+        System.out.println("Total of Odd number: "+totalOdd);
+    }
+}
