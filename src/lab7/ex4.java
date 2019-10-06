d750c8b6f4968bdb899c4a7735fad9761d25fd23
Piyada <Piyadakh@nvc.ac.th>
2019-10-06 11:22:15 +0700

    lab7

---
+++ src/lab7/ex4.java
@@ -0,0 +1,18 @@
+package lab7;
+
+public class ex4 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.print("hello.");
+                System.out.println("\tPRAN");
+            }else
+                System.out.println("hello");
+
+
+        }//man
+
+
+    }
+}//class
